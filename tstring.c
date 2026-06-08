b53ebb53ffef2cfdf98481b2849e11bc32b33a8f
davidly <davidly_lee@hotmail.com>
2026-06-08 16:32:06 -0700

    add performance benchmark

---
+++ tstring.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+char ac[ 4096 ];
+char other[ 4096 ];
+char zeroes[ 4096 ]; // will be put in bss and guaranteed to be 0 by C89 and later
+
+#define _countof( X ) ( sizeof( X ) / sizeof( X[0] ) )
+
+int main( int argc, char * argv[] )
+{
+    char alpha[27];
+    int loops, i, slen;
+    int loop_count = ( argc > 1 ) ? atoi( argv[ 1 ] ) : 1;
+
+    for ( loops = 0; loops < loop_count; loops++ )
+    {
+        srand( 317 );
+        for ( i = 0; i < sizeof( ac ); i++ )
+            ac[ i ] = ( 'a' + ( i % 26 ) );
+
+        printf( "testing strlen\n" );
+        for ( i = 0; i < 1000; i++ )
+        {
+            int start = ( (unsigned int) rand() % 300 );
+            int end = 1 + start + ( (unsigned int) rand() % 3000 );
+            int len = end - start;
+            char orig = ac[ end ];
+            ac[ end ] = 0;
+            slen = strlen( ac + start );
+            if ( len != slen )
+            {
+                printf( "strlen failed: iteration %d, len %d, strlen %d, start %d, end %d\n", i, len, slen, start, end );
+                exit( 1 );
+            }
+            ac[ end ] = orig;
+        }
+
+        printf( "testing strchr and strrchr\n" );
+        for ( i = 0; i < 1000; i++ )
+        {
+            char * pbang;
+            int start = ( (unsigned int) rand() % 300 ); 
+            int end = 1 + start + ( (unsigned int) rand() % 70 );
+            int len = end - start;
+            char orig = ac[ end ];
+            ac[ end ] = '!';
+            pbang = strchr( ac + start, '!' );
+            if ( !pbang )
+            {
+                printf( "strchr failed to find char: iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            if ( pbang != ( ac + end ) )
+            {
+                printf( "strchr offset incorrect: iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            pbang = strrchr( ac + start, '!' );
+            if ( !pbang )
+            {
+                printf( "strrchr failed to find char: iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            if ( pbang != ( ac + end ) )
+            {
+                printf( "strrchr offset incorrect: iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            if ( strchr( ac + start, '$' ) )
+            {
+                printf( "strrchr somehow found $: iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            ac[ end ] = orig;
+        }
+
+        printf( "testing strstr\n" );
+        strcpy( alpha, "abcdefghijklmnopqrstuvwxyz" );
+        for ( i = 0; i < 1000; i++ )
+        {
+            const char * pattern, *p;
+            int start = ( (unsigned int) rand() % 300 );
+            int offset = ( (unsigned int) rand() % 26 );
+            int len = 1 + ( (unsigned int) rand() % ( 26 - offset ) );
+            if ( ( offset + len ) > 26 )
+            {
+                printf( "test bug offset %d, len %d\n", offset, len );
+                exit( 1 );
+            }
+            pattern = alpha + offset;
+            p = strstr( ac + start, pattern );
+            if ( !p )
+            {
+                printf( "strstr pattern not found iteration %d, start %d, offset %d, len %d, pattern %s\n", i, start, offset, len, pattern );
+                exit( 1 );
+            }
+            if ( memcmp( p, pattern, len ) )
+            {
+                printf( "strstr found the wrong pattern iteration %d, start %d, offset %d, len %d, pattern %s\n", i, start, offset, len, pattern );
+                exit( 1 );
+            }
+            if ( strstr( ac + start, "gfe" ) )
+            {
+                printf( "strstr somehow found gfe. iteration %d, start %d, offset %d\n", i, start, offset );
+                exit( 1 );
+            }
+        }
+
+        printf( "testing memcpy and memcmp\n" );
+        for ( i = 0; i < 1000; i++ )
+        {
+            int start = ( (unsigned int) rand() % 300 );
+            int end = 1 + start + ( (unsigned int) rand() % 3000 );
+            int len = end - start;
+
+            memcpy( other + start, ac + start, len );
+            if ( memcmp( other + start, ac + start, len ) )
+            {
+                printf( "memcmp of memcpy'ed memory failed to find match, iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+            memset( other + start, 0, len );
+            if ( memcmp( other + start, zeroes + start, len ) )
+            {
+                printf( "zeroes not found in zero-filled memory, iteration %d, len %d, start %d, end %d\n", i, len, start, end );
+                exit( 1 );
+            }
+        }
+
+        printf( "testing printf\n" );
+        for ( i = 0; i < 20; i++ )
+        {
+            int start = ( (unsigned int) rand() % 300 );
+            int end = 1 + start + ( ( (unsigned int) rand() ) % 70 );
+            int len = end - start;
+            char orig = ac[ end ];
+            ac[ end ] = 0;
+
+            int l = strlen( ac + start );
+            printf( "%2d (%2d): %s\n", len, l, ac + start );
+            ac[ end ] = orig;
+        }
+
+        #ifdef TEST_WCHAR_T
+            test_wide();
+        #endif
+    }
+
+    printf( "tnarrow completed with great success\n" );
+    return 0;
+} //main

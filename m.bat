9b19378195f2595b546f9b9a9abca21e930749e8
davidly <davidly_lee@hotmail.com>
2026-06-12 11:41:56 -0700

    build dcc from src\dcc

--- m.bat
+++ m.bat
@@ -4,7 +4,11 @@ setlocal
 rem call "c:\program files\microsoft visual studio\18\community\vc\auxiliary\build\vcvars64.bat"
 call "c:\program files\microsoft visual studio\2022\community\vc\auxiliary\build\vcvars64.bat"
 
-cl /nologo dcc.c /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /D_AMD64_ /link user32.lib ntdll.lib /OPT:REF
+pushd src\dcc
+call build-dcc.bat
+popd
+
+rem cl /nologo dcc.c /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /D_AMD64_ /link user32.lib ntdll.lib /OPT:REF
 
 cl /nologo dccpeep.c /GS- /GL /Oti2 /Ob3 /Qpar /Fa /FAsc /EHac /Zi /D_AMD64_ /link user32.lib ntdll.lib /OPT:REF
 
